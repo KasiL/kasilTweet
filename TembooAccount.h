2957678386f3fb710f31e7f1dc983ec4f3410bf8
Casey <kasilapp@gmail.com>
2014-10-30 12:56:52 -0500

    Create TembooAccount.h
    Need this authentication file for the kasilreadtweet sketch

---
+++ TembooAccount.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "kasilapp"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "3b1124c945d54111bb69a428d1c3a220"  // your Temboo app key
